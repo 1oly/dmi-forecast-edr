7623f0f769ba5232184ef125ee52592b360156a2
Oliver Lylloff <ollyl@dtu.dk>
2024-04-09 14:28:59 +0200

    update version

--- dmi_forecast_edr/__init__.py
+++ dmi_forecast_edr/__init__.py
@@ -1,6 +1,6 @@
 """Python interface to DMI forecast data API"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from dmi_forecast_edr.enums import Collection
 from dmi_forecast_edr.client import DMIForecastEDRClient
